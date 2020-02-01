0b2f34d00f9d0ebc8779bec665ce4dc13e40f438
Isaac <s10177952@connect.np.edu.sg>
2020-02-01 11:31:34 +0800

    Update eti-maze-game.py
    added configuration menu

--- eti-maze-game.py
+++ eti-maze-game.py
@@ -1,9 +1,21 @@
-menu=(' \nMAIN MENU \n==========\
+menu=(' \nMAIN MENU \n=========\
 \n[1]  Read and load maze from file \n[2]  \
 View maze \n[3]  Play maze game \
  \n[4]  Configure current maze \n \n[0]  Exit Maze\n')
 
+configMenu=(' \nCONFIGURATION MENU \n=================\
+\n[1]  Create wall \n[2]  \
+Create passageway \n[3]  Create start point \
+ \n[4]  Create end point \n \n[0]  Exit to Main Menu\n')
 
+def configurationMenu():
+    print(configMenu)
+    print()
+    option=input('Enter your option:')
+    print()
+    if option =='0':
+        input('')
+    mainMenu()
 def mainMenu():
     #create a function for main menu to allow for repetition 
     print(menu)
@@ -13,11 +25,13 @@ def mainMenu():
     if option=='1':
         print('Option 1: Read and load maze from file')
         print()
+        name=str(input('Enter the name of the data file:'))
         input('')
+        print('Number of lines read:')
         mainMenu()
         
     elif option=='2':
-        print('Option 2: View maze')       
+        print('Option 2: View Maze')       
         input('')
         mainMenu()
 
@@ -28,8 +42,8 @@ def mainMenu():
 
     elif option=='4':
         print('Option 4: Configure current maze')
-        input('')
-        mainMenu()
+        configurationMenu()
+
         
     elif option=='0':
         print('Bye!')
