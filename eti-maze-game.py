c71360000747ee25cf001534be03b0545aebaa41
Isaac <s10177952@connect.np.edu.sg>
2020-02-01 12:00:22 +0800

    configuration menu & mainmenu update
    Update

--- eti-maze-game.py
+++ eti-maze-game.py
@@ -3,7 +3,7 @@ menu=(' \nMAIN MENU \n=========\
 View maze \n[3]  Play maze game \
  \n[4]  Configure current maze \n \n[0]  Exit Maze\n')
 
-configMenu=(' \nCONFIGURATION MENU \n=================\
+configMenu=(' \nCONFIGURATION MENU \n==================\
 \n[1]  Create wall \n[2]  \
 Create passageway \n[3]  Create start point \
  \n[4]  Create end point \n \n[0]  Exit to Main Menu\n')
@@ -13,41 +13,51 @@ def configurationMenu():
     print()
     option=input('Enter your option:')
     print()
-    if option =='0':
+    if option=='1':
+        input()
+        configurationMenu()
+    elif option=='2':
+        input()
+        configurationMenu()
+    elif option=='3':
+        input()
+        configurationMenu()
+    elif option=='4':
+        input()
+        configurationMenu()
+    elif option =='0':
+        print()
+        mainMenu()
+    else:   
+        print('Invalid option. Please try again.')
         input('')
-    mainMenu()
+        configurationMenu()
+    
 def mainMenu():
     #create a function for main menu to allow for repetition 
     print(menu)
     option=input('Enter your option:')
     print()
-
     if option=='1':
         print('Option [1] Read and load maze from file')
         print()
         name=str(input('Enter the name of the data file:'))
         print('Number of lines read:')
         input('')
-        mainMenu()
-        
+        mainMenu()        
     elif option=='2':
         print('Option [2] View Maze')       
         input('')
         mainMenu()
-
     elif option=='3':
         print('Option [3] Play maze game')
         input('')
         mainMenu()
-
     elif option=='4':
         print('Option [4] Configure current maze')
-        configurationMenu()
-
-        
+        configurationMenu()     
     elif option=='0':
         print('Bye!')
-
     else:
         print('Invalid option. Please try again.')
         input('')
